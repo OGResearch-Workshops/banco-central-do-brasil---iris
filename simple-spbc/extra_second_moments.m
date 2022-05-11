ce020abbb50b4aeafebf1578a7a5e8b2956e34fc
Jaromir Benes <jaromir.benes@gmail.com>
2022-05-11 16:35:48 +0200

    Second moments

--- simple-spbc/extra_second_moments.m
+++ simple-spbc/extra_second_moments.m
@@ -26,3 +26,19 @@ disp("Correlation submatrices for log_N, log_dP, log_R")
 R(["log_N", "log_dP", "log_R"], ["log_N", "log_dP", "log_R"], :)
 
 
+freq = linspace(0, pi, 1000);
+[S, D] = xsf(m, freq);
+
+s1 = S("log_Y", "log_Y", :);
+plot(freq, s1);
+title("Power spectrum for output");
+set(gca(), 'XLim', [2*pi/40, pi]);
+
+
+% ACF for a band of frequencies betwen 4 and 40 quarters
+[C1, R1] = acf(m, order=1, filter='per>=4 & per<=40');
+
+% ACF for all model variables subjected to a 1,600 Hp
+[C2, R2] = acf(m, order=1, filter='1600/(1600 + 1/abs((1-L)^2)^2)');
+
+
